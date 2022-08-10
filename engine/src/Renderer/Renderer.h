bffd1667170f527d35f41b60858177a984a0e8d4
Fix <fhomolka@protonmail.com>
2022-08-10 22:21:12 +0200

    :recycle: Give a camera to the renderer

--- engine/src/Renderer/Renderer.h
+++ engine/src/Renderer/Renderer.h
@@ -1,10 +1,12 @@
 #ifndef CBE_3D_RENDERER
 #define CBE_3D_RENDERER
 
+#include "Camera.h"
+
 #include "glm/vec4.hpp"
 #include "glm/mat4x4.hpp"
-
 #include "SDL.h"
+
 #include <array>
 
 namespace CBE
@@ -22,7 +24,7 @@ namespace CBE
 		void DrawTri();
 
 		void SetClearColor(glm::vec4&& newColor);
-
+		Camera camera;
 	private:
 		SDL_GLContext m_context;
 		glm::mat4 m_viewProjectionMat;
