0135ca2537bae90c343d7cf40b901f2fcd502ece
Fix <fhomolka@protonmail.com>
2022-08-12 19:24:59 +0200

    :recycle: silence warnings about unused parameters for compiling main.cpp

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -2,6 +2,9 @@
 #include "App.h"
 
 int main(int argc, char* argv[]) {
+	//TEMP(fhomolka): Until we actually do something with them, silence unused warnings
+	(void)argc;
+	(void)argv;
 	CBE::App game;
 	game.Loop();
 	return 0;
