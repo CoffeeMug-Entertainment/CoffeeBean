802b7309a07798d681f28cd4e0937aaff72954fc
Fix <fhomolka@protonmail.com>
2024-10-04 11:09:42 +0200

    build.sh: add commented out build mode for the game

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ mkdir -p ./build
 echo "Building Engine"
 odin build engine -out=./build/coffeebean -collection:libs="./libs" -debug -show-timings $@
 
+#echo "Building Basegame"
+#odin build basegame/src -out=./basegame/game -build-mode:dll -debug -show-timings $@
+
 # Thanks @ReformedJoe for the name
 echo "Building Map Compiler"
 odin build map_compiler -out=./build/percolator -collection:libs="./libs" -debug -show-timings $@
